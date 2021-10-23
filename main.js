858228c0b8fce9a83be5e80463953cf69ab81fc4
Jonas Carvalho <jonasmcarvalho@outlook.com>
2021-10-23 09:05:25 -0300

    Add files via upload

---
+++ main.js
@@ -0,0 +1,43 @@
+const linksSocialMedia = {
+    github: 'jonasmcarvalho',
+    youtube: 'channel/UCqLeXR,dARtCBln3bHCJbojw',
+    facebook: 'jonas.carvalho',
+    instagram: 'carvalhojonas_',
+    twitter: 'maykbrito'
+}
+
+function changeSocialMediaLinks() {
+    /* document.getElementById('userName').
+    textContent = 'Olivia' */
+
+    /* userName.textContent = "Marcelo" //vantagem de usar
+    //id, colocando o id.ocomandonafrente -> ele já entende
+    //que vc quer manipular a tag com aquele id */
+
+    for(let li of socialLinks.children) {
+        const social = li.getAttribute('class')
+
+        li.children[0].href = `https://${social}.com/${linksSocialMedia[social]}`
+    }
+
+
+}
+
+changeSocialMediaLinks()
+
+function getGitHubProfileInfos(){
+    const url = `https://api.github.com/users/${linksSocialMedia.github}`
+
+    fetch(url)
+        .then(response => response.json())
+        .then(data => {
+            userName.textContent = data.name
+            userBio.textContent = data.bio
+            userGit.href = data.html_url
+            userImage.src = data.avatar_url
+            userLogin.textContent = data.login
+        })
+}
+
+getGitHubProfileInfos()
+
